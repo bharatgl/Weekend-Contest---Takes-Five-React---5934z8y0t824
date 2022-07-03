a3c12e0fe0768ec86809b626c797ce6af11edeb9
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-07-03 14:01:29 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import '../styles/App.css';
+import "../styles/App.css";
 const App = () => {
-  
+  const [number, setNumber] = useState(1);
+
+  const LoadNumbers = ({ number }) => {
+    let divs = [];
+    for (let i = number; i < number + 5; i++) {
+      divs.push(i);
+    }
+
+    return (
+      <>
+        {divs.map((d) => {
+          return <div>{d}</div>;
+        })}
+      </>
+    );
+  };
 
   return (
     <div id="main">
-
-      <div>
-
-      </div>
+      <div>{<LoadNumbers number={number} />}</div>
+      <button
+        id="back-button"
+        onClick={() => setNumber(number - 5)}
+        disabled={number == 1 ? true : false}
+      >
+        Prev Page
+      </button>
+      <button id="next-button" onClick={() => setNumber(number + 5)}>
+        Next Page
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
